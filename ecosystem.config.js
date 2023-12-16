4611b7e04f29bc0bfdcac0f1a51a8f4845fbdeda
simplyno <simplehno@gmail.com>
2023-12-16 05:40:42 +0100

    log

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,10 @@ module.exports = {
   apps: [{
     script: 'index.js',
     name: "HGB",
+    max_memory_restart: '2G',
     args: [
       "--color"
-    ]
+    ],
+    log_date_format: "YYYY-MM-DD HH:mm Z"
   }],
 };
