4894834f5660c981d6ffe677d93bc95f3f500d1a
simplyno <simplehno@gmail.com>
2024-06-07 17:46:37 +0000

    add 6th slot in autopost slash command

--- bot/commands/config/autopost.js
+++ bot/commands/config/autopost.js
@@ -51,7 +51,7 @@ module.exports = {
                 .addIntegerOption(option =>
                     option
                         .setName('slot')
-                        .addChoices({ name: '1', value: 1 }, { name: '2', value: 2 }, { name: '3', value: 3 }, { name: '4', value: 4 }, { name: '5', value: 5 })
+                        .addChoices({ name: '1', value: 1 }, { name: '2', value: 2 }, { name: '3', value: 3 }, { name: '4', value: 4 }, { name: '5', value: 5 }, { name: '6', value: 6 })
                         .setDescription('Slot to set.')
                         .setRequired(true))
 
