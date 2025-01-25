728892458d718bd56af7198ea4797e2d164dbde6
SimplyNo <simplehno@gmail.com>
2025-01-25 05:30:55 +0000

    autopost fix slot not working

--- bot/commands/config/autopost.js
+++ bot/commands/config/autopost.js
@@ -63,7 +63,7 @@ module.exports = {
                 .addIntegerOption(option =>
                     option
                         .setName('slot')
-                        .addChoices({ name: '1', value: 1 }, { name: '2', value: 2 }, { name: '3', value: 3 }, { name: '4', value: 4 }, { name: '5', value: 5 })
+                        .addChoices({ name: '1', value: 1 }, { name: '2', value: 2 }, { name: '3', value: 3 }, { name: '4', value: 4 }, { name: '5', value: 5 }, { name: '6', value: 6 })
                         .setDescription('Slot to set.')
                         .setRequired(true))
                 .addChannelOption(option =>
@@ -87,7 +87,7 @@ module.exports = {
                 .addIntegerOption(option =>
                     option
                         .setName('slot')
-                        .addChoices({ name: '1', value: 1 }, { name: '2', value: 2 }, { name: '3', value: 3 }, { name: '4', value: 4 }, { name: '5', value: 5 })
+                        .addChoices({ name: '1', value: 1 }, { name: '2', value: 2 }, { name: '3', value: 3 }, { name: '4', value: 4 }, { name: '5', value: 5 }, { name: '6', value: 6 })
                         .setDescription('Slot to delete.')
                         .setRequired(true)))
         .addSubcommand(subcmd =>
